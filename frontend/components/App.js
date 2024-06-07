1e636692b51b7b6978043a348b84f4382a34ef21
David S. <neojohn2312@gmail.com>
2024-06-07 16:20:40 -0500

    FirstPush

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import  Form  from './Form'
+import TodoList from './TodoList'
+
+let id = 0;
+let getId = () => ++id;
+
+const initialTodoes = [
+  { id: getId(), name: "Walk the dog", completed: false },
+  { id: getId(), name: "Learn React", completeed: true },
+  { id: getId(), name: "Have Fun", completed: false },
+]
 
 export default class App extends React.Component {
+  state = {
+    todos: initialTodoes
+  }
+
+  addTodo = (name) => {
+    this.setState ({
+      ...this.state,
+      todos: this.state.todos.concat({ id: getId(), completed: false, name })
+    })
+  }
+
+  toggleCompletion = id => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(td => {
+        if (id == td.id) return { ...td, completed: !td.completed}
+        return td;
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todos={this.state.todos} toggleCompletion={this.toggleCompletion} />
+        <Form addTodo={this.addTodo} />
       </div>
     )
   }
